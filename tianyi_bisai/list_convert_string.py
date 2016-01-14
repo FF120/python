ee978ff1dde758c46b8dfdb1804cc547409dd246
FF120 <gxf120406191@gmail.com>
2016-01-14 18:16:28 +0800

    tianyi_code_one

---
+++ tianyi_bisai/list_convert_string.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 15:30:51 2016
+
+@author: FF120
+"""
+
+def list_to_str(list):
+    str1 = str(list)
+    str1 = str1.replace(']','').replace('[','').replace(' ','')
+    return str1
+
+for line in open('e:/test_sigmoid222.txt','r'):
+    aa =  line.strip('\n')  .split('\t');
+    bb = map(int,aa[1].split(','));
+    cc = []
+    maxValues = max(bb)
+    minValues = min(bb)
+    for x in bb:
+        y = (float)(x-minValues)/(maxValues-minValues)
+        y = (int)(y*1000)
+        cc.append(y)
+    with open('e:/test_sigmoid.txt','a') as of:
+        outstr = aa[0]
+        outstr = outstr + "\t"
+        outstr = outstr + list_to_str(cc)
+        outstr = outstr + "\n"
+        of.write(outstr)
